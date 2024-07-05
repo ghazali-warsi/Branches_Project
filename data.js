97e1089cfae896f95549048c585600e427a29c0d
Ghazali-Warsi <warsighazali125@gmail.com>
2024-07-05 20:25:24 +0500

    some

---
+++ data.js
@@ -0,0 +1,82 @@
+var product = [
+    {
+        "id" : 0,
+        "title":"",
+        "price":"",
+        "description":"",
+        "brand":"",
+        "image":"Images/No-Image-Placeholder.svg.png"
+    },
+    {
+        "id" : 1,
+        "title":"Handbag-1",
+        "price":"100$",
+        "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea natus dolor expedita reiciendis nulla recusandae quasi, architecto at maiores earum omnis cupiditate nemo quam. Et sequi nam odio labore in.",
+        "brand":"Lous vuitton",
+        "image":"Images/1.avif"
+    },
+    {
+        "id" : 2,
+        "title":"Handbag-2",
+        "price":"150$",
+        "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea natus dolor expedita reiciendis nulla recusandae quasi, architecto at maiores earum omnis cupiditate nemo quam. Et sequi nam odio labore in.",
+        "brand":"Lous vuitton",
+        "image":"Images/2.avif"
+    },
+    {
+        "id" : 3,
+        "title":"Handbag-3",
+        "price":"800$",
+        "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea natus dolor expedita reiciendis nulla recusandae quasi, architecto at maiores earum omnis cupiditate nemo quam. Et sequi nam odio labore in.",
+        "brand":"Lous vuitton",
+        "image":"Images/3.avif"
+    },
+    {
+        "id" : 4,
+        "title":"Handbag-4",
+        "price":"600$",
+        "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea natus dolor expedita reiciendis nulla recusandae quasi, architecto at maiores earum omnis cupiditate nemo quam. Et sequi nam odio labore in.",
+        "brand":"Lous vuitton",
+        "image":"Images/4.avif"
+    },
+    {
+        "id" : 5,
+        "title":"Handbag-5",
+        "price":"400$",
+        "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea natus dolor expedita reiciendis nulla recusandae quasi, architecto at maiores earum omnis cupiditate nemo quam. Et sequi nam odio labore in.",
+        "brand":"Lous vuitton",
+        "image":"Images/5.avif"
+    },
+    {
+        "id" : 6,
+        "title":"Handbag-6",
+        "price":"300$",
+        "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea natus dolor expedita reiciendis nulla recusandae quasi, architecto at maiores earum omnis cupiditate nemo quam. Et sequi nam odio labore in.",
+        "brand":"Lous vuitton",
+        "image":"Images/6.avif"
+    },
+    {
+        "id" : 7,
+        "title":"Handbag-7",
+        "price":"180$",
+        "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea natus dolor expedita reiciendis nulla recusandae quasi, architecto at maiores earum omnis cupiditate nemo quam. Et sequi nam odio labore in.",
+        "brand":"Lous vuitton",
+        "image":"Images/7.avif"
+    },
+    {
+        "id" : 8,
+        "title":"Handbag-8",
+        "price":"160$",
+        "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea natus dolor expedita reiciendis nulla recusandae quasi, architecto at maiores earum omnis cupiditate nemo quam. Et sequi nam odio labore in.",
+        "brand":"Lous vuitton",
+        "image":"Images/8.avif"
+    },
+    {
+        "id" : 9,
+        "title":"Handbag-5",
+        "price":"400$",
+        "description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea natus dolor expedita reiciendis nulla recusandae quasi, architecto at maiores earum omnis cupiditate nemo quam. Et sequi nam odio labore in.",
+        "brand":"Lous vuitton",
+        "image":"Images/9.avif"
+    },
+]
